8d5ef62a80f3542ba4a17fad6ea4d9feac9fd4aa
wongkyunban <wongkyunban@sina.com>
2020-01-03 14:58:25 +0800

    Initial commit 模块化开发

---
+++ modulea/src/main/java/com/wong/modulea/WarehouseActivity.java
@@ -0,0 +1,14 @@
+package com.wong.modulea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WarehouseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warehouse);
+    }
+}
